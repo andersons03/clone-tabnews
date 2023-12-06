9b3eb7cd4aefa6f4e066af2e6074b46155f8702c
andersons03 <125624411+andersons03@users.noreply.github.com>
2023-12-06 22:37:11 +0000

    atualizano pequenos ajustes

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto de clone do tabnews.com.br, feito no curso.dev do Filipe Deschamps
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-function Home(){
-    return <h1>Olá <s>github!</s> Vercel!👋👋</h1>
+function Home() {
+    return (
+        <h1>
+            Olá <s>github!</s> Vercel!👋👋
+        </h1>
+    );
 }
 
 export default Home;
