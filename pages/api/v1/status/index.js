c5a31269d2708d0647f217038487dd1404a7f734
andersons03 <125624411+andersons03@users.noreply.github.com>
2024-05-22 00:58:20 +0000

    Ajustes

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,10 @@
-function status(request,response){
+import database from "../../../../infra/database.js";
+
+async function status(request,response){
     // response.status(200).send("Olá, eu estou em uma API");
+    // console.log(database());
+    const result = await database.query('SELECT 1 + 1 as sum');
+    console.log(result.rows);
     response.status(200).json({"mensagem" : "Olá, eu estou em uma API, agora em JSON"});
 }
 
