8468d1c442541f9517c28682929b2fecc4d92dac
Anderson <anderson.ss.dev@gmail.com>
2025-05-21 19:18:05 -0300

    add `jsconfig.json`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,4 @@
-import query from "../../../../infra/database";
+import query from "infra/database";
 
 async function status(request, response) {
   const result = await query("SELECT 1 + 1 AS Soma");
