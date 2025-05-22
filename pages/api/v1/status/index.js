147040b7afc9d2c5173c00e8bf3ca858b852c741
Anderson <anderson.ss.dev@gmail.com>
2025-05-22 00:24:09 -0300

    atualizacao da api de status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,29 @@
 import query from "infra/database";
 
 async function status(request, response) {
-  const result = await query("SELECT 1 + 1 AS Soma");
-  console.log(result.rows);
-  response.status(200).json({ message: "não sei o que escrever" });
+  const updateAt = new Date().toISOString();
+  const dbVersionResult = await query("SHOW server_version;");
+  const dbVersionValue = dbVersionResult.rows[0].server_version;
+  const dbMaxConnectionResult = await query("SHOW max_connections;");
+  const dbMaxConnectionValue = dbMaxConnectionResult.rows[0].max_connections;
+
+  const dbOpenedConnectionsquery = {
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname=$1;",
+    values: [process.env.POSTGRES_DB],
+  };
+  const dbOpenedConnectionsResult = await query(dbOpenedConnectionsquery);
+  const dbOpenedConnectionsValue = dbOpenedConnectionsResult.rows[0].count;
+
+  response.status(200).json({
+    update_at: updateAt,
+    dependencies: {
+      database: {
+        max_connections: parseInt(dbMaxConnectionValue),
+        opened_connections: dbOpenedConnectionsValue,
+        version: dbVersionValue,
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integrations/api/v1/status/get.test.js
+++ tests/integrations/api/v1/status/get.test.js
@@ -1,4 +1,16 @@
 test("Get status must be 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
+  const responseBody = await response.json();
+  const updateAt = responseBody.update_at;
+  console.log(responseBody);
+
   expect(response.status).toBe(200);
+
+  expect(responseBody.dependencies.database.version).toBe("16.9");
+
+  expect(responseBody.dependencies.database.max_connections).toBe(100);
+  expect(responseBody.dependencies.database.opened_connections).toBe(1);
+
+  const parseUpdateAt = new Date(updateAt).toISOString();
+  expect(updateAt).toEqual(parseUpdateAt);
 });
